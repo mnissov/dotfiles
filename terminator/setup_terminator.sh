4eabb8993c41b5397d6005d25ee27668b0304680
Morten <morten.c.nissov@gmail.com>
2022-04-07 15:10:07 +0200

    adding termiantor setup

---
+++ terminator/setup_terminator.sh
@@ -0,0 +1,10 @@
+echo "Installing terminator"
+if lsb_release -is | grep -qi "EndeavourOS" ; then
+	sudo pacman -S terminator
+fi
+if lsb_release -is | egrep -qi "Ubuntu|Pop" ; then
+	sudo apt-get install terminator
+fi
+echo "Setting config"
+mkdir -p ~/.config/terminator
+ln -sfn ~/repos/dotfiles/terminator/config ~/.config/terminator
