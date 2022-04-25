aeb0f8a134d474fbe0214da8e6e017285fc0ec7e
Morten Nissov <morten.c.nissov@gmail.com>
2022-04-25 22:27:23 +0200

    option for installing

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-echo "Installing vim"
-if lsb_release -is | grep -qi "EndeavourOS" ; then
-	sudo pacman -S vim
-fi
-if lsb_release -is | egrep -qi "Ubuntu|Pop" ; then
-	sudo apt-get install vim
+read -p "Do you want to install vim? [Y/n]: " answer
+## Set the default value if no answer was given
+answer=${answer:Y}
+## If the answer matches y or Y, install
+if [[ $answer =~ [Yy] ]]
+then
+	if lsb_release -is | grep -qi "EndeavourOS"
+	then
+		sudo pacman -S vim
+	fi
+	if lsb_release -is | egrep -qi "Ubuntu|Pop"
+	then
+		sudo apt-get install vim
+	fi
 fi
 
 echo "Adding settings"
